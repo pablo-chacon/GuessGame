85f642d0b50770144a791157dbb93af1c2136923
Abbe-Sah <abbe.sahragard@gmail.com>
2022-11-29 13:49:30 +0100

    The files

---
+++ GuessGame/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.guessgameschool.guessgame {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    opens com.guessgameschool.guessgame to javafx.fxml;
+    exports com.guessgameschool.guessgame;
+}
