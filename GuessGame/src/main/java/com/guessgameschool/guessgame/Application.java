bcb54a92b22030b9dc96d638552d4f034505ad78
Abbe-Sah <abbe.sahragard@gmail.com>
2022-11-30 15:11:02 +0100

    update on code

--- GuessGame/src/main/java/com/guessgameschool/guessgame/Application.java
+++ GuessGame/src/main/java/com/guessgameschool/guessgame/Application.java
@@ -3,15 +3,16 @@ package com.guessgameschool.guessgame;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.control.TextArea;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 650);
+        stage.setResizable(false);
+        stage.setTitle("The Guess Game");
         stage.setScene(scene);
         stage.show();
     }
