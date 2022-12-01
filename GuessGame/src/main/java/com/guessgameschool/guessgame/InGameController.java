041de346889c4fa7c6a1298bbcef91229cb5d7f0
Abbe Sahragard <92218444+Abbe-Sah@users.noreply.github.com>
2022-12-01 13:44:18 +0100

    Update InGameController.java

--- GuessGame/src/main/java/com/guessgameschool/guessgame/InGameController.java
+++ GuessGame/src/main/java/com/guessgameschool/guessgame/InGameController.java
@@ -2,14 +2,86 @@ package com.guessgameschool.guessgame;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 public class InGameController {
     @FXML
-    private Label welcomeText;
+    private Label questionBox;
+    @FXML
+    private Label countDownTimer;
+    @FXML
+    private RadioButton lvl1;
+    @FXML
+    private RadioButton lvl2;
+    @FXML
+    private RadioButton lvl3;
+    @FXML
+    private RadioButton lvl4;
+    @FXML
+    private RadioButton lvl5;
+    @FXML
+    private RadioButton lvl6;
+    @FXML
+    private RadioButton lvl7;
+    @FXML
+    private RadioButton lvl8;
+    @FXML
+    private RadioButton lvl9;
+    @FXML
+    private RadioButton lvl10;
+    @FXML
+    private RadioButton lvl11;
+    @FXML
+    private RadioButton lvl12;
+    @FXML
+    private RadioButton lvl13;
+    @FXML
+    private RadioButton lvl14;
+    @FXML
+    private RadioButton lvl15;
+    @FXML
+    private RadioButton lvl16;
+    @FXML
+    private RadioButton lvl17;
+    @FXML
+    private RadioButton lvl18;
+    @FXML
+    private RadioButton lvl19;
+    @FXML
+    private RadioButton lvl20;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void optionOne() {
+        questionBox.setText("You pressed button nr one");
+
+    }
+    @FXML
+    protected void optionTwo() {
+        questionBox.setText("You pressed button nr two");
+
+    }
+    @FXML
+    protected void optionThree() {
+        questionBox.setText("You pressed button nr three");
+
+    }
+    @FXML
+    protected void optionFour() {
+        questionBox.setText("You pressed button nr four");
+
+    }
+
+    @FXML
+    protected void stopPlay() {
+        questionBox.setText("Stop playing");
+
+    }
+
+    @FXML
+    protected void exitGame() {
+        questionBox.setText("Exit game");
 
     }
 }
