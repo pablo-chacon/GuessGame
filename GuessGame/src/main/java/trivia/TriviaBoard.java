e7f0477d7c54e8dfbcf24a4f1a284d5b15e17559
pablo-chacon <Snemilio@protonmail.com>
2022-12-02 22:27:41 +0100

    trivia + game board

--- GuessGame/src/main/java/trivia/TriviaBoard.java
+++ GuessGame/src/main/java/trivia/TriviaBoard.java
@@ -5,11 +5,14 @@ import java.util.*;
 public class TriviaBoard implements Trivia<Subject> {
 
     List<String> triviaQueries = new ArrayList<>(20);
-    List<String> triviaResponses = new ArrayList<>(20);
+    Subject subject = new Subject();
 
     public TriviaBoard() {
-        triviaQueries.add(new Subject().Astronomy().get(2));
-        triviaQueries.add(new Subject().History().get(5));
+        triviaQueries.add(subject.Astronomy().get(2));
+        triviaQueries.add(subject.History().get(5));
+        triviaQueries.add(subject.Computers().get(9));
+        triviaQueries.add(subject.Games().get(7));
+        triviaQueries.add(subject.Geography().get(3));
     }
 
     @Override
